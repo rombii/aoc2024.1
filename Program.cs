86f5ae9fa71d2be8f35d1ff8b7b46b87397bfae8
Kamil Kondziolka <wono83@interia.pl>
2024-12-01 22:35:25 +0100

    Better path management

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using var inputReader = new StreamReader(Path.Join(Directory.GetCurrentDirectory(), "input.txt"));
+long ansPart1 = 0, ansPart2 = 0;
+PriorityQueue<int, int> queue1 = new(), queue2 = new();
+var part2Dict = new Dictionary<int, int>();
+while (!inputReader.EndOfStream)
+{
+    var line = await inputReader.ReadLineAsync();
+    var splitLine = line!.Split("   ");
+    if (int.TryParse(splitLine[0], out var item1) && int.TryParse(splitLine[1], out var item2))
+    {
+        queue1.Enqueue(item1, item1);
+        queue2.Enqueue(item2, item2);
+        if (part2Dict.TryGetValue(item2, out var value))
+        {
+            part2Dict[item2] = ++value;
+        }
+        else
+        {
+            part2Dict[item2] = 1;
+        }
+    }
+}
 
-Console.WriteLine("Hello, World!");
+while (queue1.Count > 0 && queue2.Count > 0)
+{
+    int item1 = queue1.Dequeue(), item2 = queue2.Dequeue();
+    ansPart1 += Math.Abs(item1 - item2);
+    if (!part2Dict.ContainsKey(item1))
+    {
+        continue;
+    }
+    ansPart2 += item1 * part2Dict[item1];
+}
+
+Console.WriteLine($"First part: {ansPart1}");
+Console.WriteLine($"Second part: {ansPart2}");
